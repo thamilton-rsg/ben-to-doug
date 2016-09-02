5ac9fa00c2598061055e678da3eadb8bce384122
Matthew Billings <mbillings@RSG-5322-Matthew-Billings.local>
2016-09-02 09:57:42 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+Ben To Doug
+=============
+
+Chrome extension that replaces occurrences of 'Ben Chestnut' with 'Doug Chestnut'. I could have made it swap "Ben" with "Doug", but it would have resulted in some false positives and taken away from the lulz factor. 
+
+[Direct download of crx file](https://github.com/easement/ben-to-doug/blob/master/BenToDoug.crx?raw=true)
+
+
+Installation
+------------
+
+In Chrome, choose Window > Extensions.  Drag BenToDoug.crx into the page that appears.
+
+
+Thanks
+------
+Thanks to @panicsteve for doing all the hard work with his cloud-to-butt suff.
+https://github.com/panicsteve/cloud-to-butt
---
+++ Source/content_script.js
@@ -0,0 +1,40 @@
+walk(document.body);
+
+function walk(node) {
+	// I stole this function from here:
+	// http://is.gd/mwZp7E
+	var child, next;
+	
+	switch (node.nodeType) {
+		case 1:  // Element
+		case 9:  // Document
+		case 11: // Document fragment
+			child = node.firstChild;
+			while (child) {
+				next = child.nextSibling;
+				walk(child);
+				child = next;
+			}
+			break;
+		case 3: // Text node
+			handleText(node);
+			break;
+	}
+}
+
+
+// This is where the magic happens
+function handleText(textNode) 
+{
+	var v = textNode.nodeValue;
+
+	v = v.replace(/\bBen Chestnut\b/g, "Doug Chestnut");
+	v = v.replace(/\Ben Chestnut\b/g, "Doug Chestnut");
+	v = v.replace(/\Ben Chestnut\b/g, "Doug Chestnut");
+	v = v.replace(/\Ben Chestnut\b/g, "Doug Chestnut");
+	v = v.replace(/\Ben Chestnut\b/g, "Doug Chestnut");
+	
+	textNode.nodeValue = v;
+}
+
+
